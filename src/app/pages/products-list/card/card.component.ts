7c4a0f3611a9d81d6dbe8c2026d6ec3b717b7250
Ivan Shutov <vanasutov793@gmail.com>
2024-01-13 19:21:23 +0400

    feat: change product-list and card components

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,19 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | undefined;
 
-    onProductBuy(event: Event) {
+    @Output() ProductBuy = new EventEmitter<IProduct>();
+
+    onProductBuy(event: MouseEvent) {
         event.stopPropagation();
+        this.ProductBuy.emit(this.product);
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return Number(this?.product?.rating) >= starIndex;
     }
 }
