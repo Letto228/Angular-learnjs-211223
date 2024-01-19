dcef5efbf4baccc60e1aa139a6fbd9da81d09bbe
nikita raevnev <nikita.raevnev@gmail.com>
2024-01-20 01:00:42 +0300

    feat: symplify

--- src/app/components/popup/popup-host/popup-host.component.ts
+++ src/app/components/popup/popup-host/popup-host.component.ts
@@ -6,19 +6,17 @@ import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angul
     styleUrls: ['./popup-host.component.css'],
 })
 export class PopupHostComponent {
-    @ViewChild(TemplateRef, {static: true})
-    private readonly popupWrapperTemplate: TemplateRef<unknown> | null = null;
-
     @ViewChild('viewport', {read: ViewContainerRef, static: true})
     private readonly viewportViewContainerRef: ViewContainerRef | null = null;
 
-    templateValue: TemplateRef<unknown> | null = null;
-    @Input() set template(value: TemplateRef<unknown> | null) {
-        this.templateValue = value;
-        this.viewportViewContainerRef?.clear();
+    isPopupHidden = true;
 
-        if (this.popupWrapperTemplate && value) {
-            this.viewportViewContainerRef?.createEmbeddedView(this.popupWrapperTemplate);
+    @Input() set template(value: TemplateRef<unknown> | null) {
+        this.viewportViewContainerRef?.clear();
+        this.isPopupHidden = !value;
+
+        if (value) {
+            this.viewportViewContainerRef?.createEmbeddedView(value);
         }
     }
 }
--- src/app/components/popup/popup-host/popup-host.module.ts
+++ src/app/components/popup/popup-host/popup-host.module.ts
@@ -1,11 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PopupHostComponent} from './popup-host.component';
-import {PopupWrapperModule} from '../popup-wrapper/popup-wrapper.module';
 
 @NgModule({
     declarations: [PopupHostComponent],
-    imports: [CommonModule, PopupWrapperModule],
+    imports: [CommonModule],
     exports: [PopupHostComponent],
 })
 export class PopupHostModule {}
