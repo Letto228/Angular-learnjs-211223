c7946c2524b0802e6eb42bebdac9affe5437c59a
Konstantin Kondaurov <konstantin.kondaurov@softgamings.com>
2024-01-21 20:00:42 +0300

    feat: add output and input properties

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,24 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | null = null;
+    @Output() readonly buyClick = new EventEmitter<string>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        if (this.product) {
+            // eslint-disable-next-line no-console
+            console.log('Send Product id to product-list:', this.product._id);
+            this.buyClick.emit(this.product._id);
+        }
+    }
+
+    get imageSrc(): string {
+        return this.product ? this.product.images[0].url : '';
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
