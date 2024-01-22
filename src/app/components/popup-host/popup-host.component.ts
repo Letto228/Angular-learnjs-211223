9f6be0a7faeade56315e9bddf4c81f1334f7b2f3
tkaryakina@abanking.ru <tkaryakina@abanking.ru>
2024-01-22 14:10:31 +0500

    fix: add popup-host component

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -1,18 +1,11 @@
-import {
-    AfterViewInit,
-    Component,
-    Input,
-    TemplateRef,
-    ViewChild,
-    ViewContainerRef,
-} from '@angular/core';
+import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
 
 @Component({
     selector: 'app-popup-host',
     templateUrl: './popup-host.component.html',
     styleUrls: ['./popup-host.component.scss'],
 })
-export class PopupHostComponent implements AfterViewInit {
+export class PopupHostComponent {
     isVisible = false;
 
     @Input() set template(template: TemplateRef<unknown> | null) {
@@ -26,10 +19,4 @@ export class PopupHostComponent implements AfterViewInit {
 
     @ViewChild('viewContainer', {read: ViewContainerRef})
     viewContainer: ViewContainerRef | null = null;
-
-    ngAfterViewInit(): void {
-        if (!this.viewContainer?.length) {
-            this.isVisible = false;
-        }
-    }
 }
