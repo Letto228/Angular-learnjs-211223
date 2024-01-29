d5128435dd0505a4b0c13919a77c7012e82d94f8
tkaryakina@abanking.ru <tkaryakina@abanking.ru>
2024-01-29 13:02:23 +0500

    fix: add popup-host component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, TemplateRef, ViewChild} from '@angular/core';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
 
 @Component({
@@ -12,19 +12,30 @@ export class AppComponent {
     switchTemplate = false;
     closeTemplate = true;
 
+    template: TemplateRef<unknown> | null = null;
+
+    @ViewChild('firstTemplate')
+    readonly firstTemplate: TemplateRef<unknown> | null = null;
+
+    @ViewChild('secondTemplate')
+    readonly secondTemplate: TemplateRef<unknown> | null = null;
+
     constructor() {
         setTimeout(() => {
             this.switchTemplate = !this.switchTemplate;
+            this.template = this.switchTemplate ? this.firstTemplate : this.secondTemplate;
             // or
             this.closeTemplate = !this.closeTemplate;
         }, 3000);
         setTimeout(() => {
             this.switchTemplate = !this.switchTemplate;
+            this.template = this.switchTemplate ? this.firstTemplate : this.secondTemplate;
             // or
             this.closeTemplate = !this.closeTemplate;
         }, 6000);
         setTimeout(() => {
             this.switchTemplate = !this.switchTemplate;
+            this.template = this.switchTemplate ? this.firstTemplate : this.secondTemplate;
             // or
             this.closeTemplate = !this.closeTemplate;
         }, 9000);
