6c8efdb1e68536c849a546303739fd3109ec862f
Irina Baleevskikh <irina.baleevskih@saritasa.com>
2024-01-31 02:12:43 +0500

    feat: add input and output to card component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,22 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input()
+    product: IProduct | null = null;
 
-    onProductBuy(event: Event) {
-        event.stopPropagation();
+    @Output()
+    readonly productIsAddedChange = new EventEmitter<IProduct['_id']>();
 
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+    protected isDisabledBuyButton = false;
+
+    onProductBuy(): void {
+        if (this.product) {
+            this.productIsAddedChange.emit(this.product._id);
+            this.isDisabledBuyButton = true;
+        }
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
