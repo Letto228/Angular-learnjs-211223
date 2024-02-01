1a3d2f84ab5b5e75e4a6f4a2e3af2f242b3ae3b7
Irina Baleevskikh <irina.baleevskih@saritasa.com>
2024-02-02 02:24:58 +0500

    feat: add popup host component

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+
+@Component({
+    selector: 'app-popup-host',
+    templateUrl: './popup-host.component.html',
+    styleUrls: ['./popup-host.component.css'],
+})
+export class PopupHostComponent {
+    @Input()
+    set template(value: TemplateRef<unknown> | null) {
+        this.popupViewContainer?.clear();
+
+        if (value !== null) {
+            this.popupViewContainer?.createEmbeddedView(value);
+        }
+    }
+
+    @ViewChild('popupViewPort', {read: ViewContainerRef})
+    protected readonly popupViewContainer?: ViewContainerRef;
+}
