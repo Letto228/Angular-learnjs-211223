8f2fbf71c9a01b4cce46c1ee8e31155803a46ba3
tkaryakina@abanking.ru <tkaryakina@abanking.ru>
2024-02-12 17:08:33 +0500

    fix: add filter pipe

--- src/app/shared/filter-by-property/filter-by-property.pipe.ts
+++ src/app/shared/filter-by-property/filter-by-property.pipe.ts
@@ -1,21 +1,28 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {isString} from 'lodash';
 
 @Pipe({
     name: 'filterByProperty',
     pure: false,
 })
 export class FilterByPropertyPipe implements PipeTransform {
-    transform<T>(
-        product: T[],
-        propertyName: string,
-        searchPropertyValue: string | number | boolean | object,
-    ): T[] {
-        if (typeof searchPropertyValue === 'string') {
+    transform<T, P extends keyof T>(
+        product: T[] | null | undefined,
+        propertyName: P,
+        searchPropertyValue: T[P],
+    ): T[] | null | undefined {
+        if (!product?.length) {
+            return product;
+        }
+
+        if (isString(searchPropertyValue)) {
+            const newSearchValue = searchPropertyValue.toUpperCase();
+
             return product.filter((item: T) =>
-                (item[propertyName as keyof T] as string).includes(searchPropertyValue),
+                (item[propertyName] as string).toUpperCase().includes(newSearchValue),
             );
         }
 
-        return product.filter((item: T) => item[propertyName as keyof T] === searchPropertyValue);
+        return product.filter((item: T) => item[propertyName] === searchPropertyValue);
     }
 }
